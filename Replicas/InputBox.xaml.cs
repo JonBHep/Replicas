630367f7bf9d65ee04e2634c21e562fe4b9ced59
Jonathan Hepworth <jonathanhepworth@hotmail.co.uk>
2022-01-13 15:10:45 +0000

    Added InputBox

---
+++ Replicas/InputBox.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace Replicas;
+
+public partial class InputBox : Window
+{
+    public InputBox()
+    {
+        InitializeComponent();
+    }
+    public InputBox(string BoxTitle, string PromptText, string DefaultResponse)
+    {
+        InitializeComponent();
+        this.Title = BoxTitle;
+        textblockPrompt.Text = PromptText;
+        textboxResponse.Text = DefaultResponse;
+    }
+
+    public string ResponseText { get { return textboxResponse.Text; } }
+
+    private void buttonOkay_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+
+    private void buttonCancel_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+    }
+
+    private void Window_ContentRendered(object sender, EventArgs e)
+    {
+        Icon = this.Owner.Icon;
+        textboxResponse.Focus();
+    }
+}
