6d3eff6ffecca736ba60eaa34536608154142ff7
Jonathan Hepworth <jonathanhepworth@hotmail.co.uk>
2022-01-13 15:14:19 +0000

    Added JbhDataReportWindow

---
+++ Replicas/JbhDataReportWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Replicas;
+
+public partial class JbhDataReportWindow : Window
+{
+    public JbhDataReportWindow()
+    {
+        InitializeComponent();
+    }
+    readonly List<string> infoList;
+        readonly List<string> businessList;
+        private readonly FontFamily _myFamily = new FontFamily("Verdana");
+        private readonly double _mySize = 14;
+        private Thickness _myThick = new Thickness(8, 3, 8, 3);
+
+        public JbhDataReportWindow(List<string> infoDates, List<string> businessDates)
+        {
+            InitializeComponent();
+            infoList = infoDates;
+            businessList = businessDates;
+            tbkHeading.FontFamily = _myFamily;
+            tbkHeading.FontSize = _mySize;
+            tbkRubric.FontFamily = _myFamily;
+            tbkRubric.FontSize = _mySize;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+
+            foreach (string s in infoList)
+            {
+                string[] part = s.Split("^".ToCharArray());
+
+                StackPanel spnl = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal
+                };
+                TextBlock tbkDate = new TextBlock()
+                {
+                    Text = part[0],
+                    FontFamily = _myFamily,
+                    FontSize = _mySize,
+                    Foreground = Brushes.Peru,
+                    Padding = _myThick,
+                    Width = 200
+                };
+                spnl.Children.Add(tbkDate);
+
+                TextBlock tbkWhen = new TextBlock()
+                {
+                    Text = part[2],
+                    FontFamily = _myFamily,
+                    FontSize = _mySize,
+                    Foreground = Brushes.SandyBrown,
+                    Padding = _myThick,
+                    Width = 120
+                };
+                spnl.Children.Add(tbkWhen);
+
+                TextBlock tbkName = new TextBlock()
+                {
+                    Text = part[1],
+                    FontFamily = _myFamily,
+                    FontSize = _mySize,
+                    Foreground = Brushes.Sienna,
+                    Padding = _myThick
+                };
+                spnl.Children.Add(tbkName);
+
+                InfoItemsControl.Items.Add(spnl);
+            }
+
+            foreach (string s in businessList)
+            {
+                string[] part = s.Split("^".ToCharArray());
+
+                StackPanel spnl = new StackPanel()
+                {
+                    Orientation = Orientation.Horizontal
+                };
+                TextBlock tbkDate = new TextBlock()
+                {
+                    Text = part[0],
+                    FontFamily = _myFamily,
+                    FontSize = _mySize,
+                    Foreground = Brushes.Peru,
+                    Padding = _myThick,
+                    Width = 200
+                };
+                spnl.Children.Add(tbkDate);
+
+                TextBlock tbkWhen = new TextBlock()
+                {
+                    Text = part[2],
+                    FontFamily = _myFamily,
+                    FontSize = _mySize,
+                    Foreground = Brushes.SandyBrown,
+                    Padding = _myThick,
+                    Width = 120
+                };
+                spnl.Children.Add(tbkWhen);
+
+                TextBlock tbkName = new TextBlock()
+                {
+                    Text = part[1],
+                    FontFamily = _myFamily,
+                    FontSize = _mySize,
+                    Foreground = Brushes.Sienna,
+                    Padding = _myThick
+                };
+                spnl.Children.Add(tbkName);
+
+                BusinessItemsControl.Items.Add(spnl);
+            }
+
+            DisplayThermometers();
+        }
+
+        private void DisplayThermometers()
+        {
+            double wid = InfoLabel.ActualWidth;
+            if (BusinessLabel.ActualWidth > wid) { wid = BusinessLabel.ActualWidth; }
+            wid += 12;
+            InfoLabel.Width = BusinessLabel.Width = wid;
+            double chartWidth = InfoPanel.ActualWidth - wid;
+            chartWidth -= 24;
+
+            List<TimeSpan> itimes = Kernel.Instance.JobProfiles.JbhInfoAgos();
+            TimeSpan most = itimes.Max();
+
+            List<TimeSpan> differences = new List<TimeSpan>();
+            TimeSpan previous = TimeSpan.Zero;
+            foreach (TimeSpan t in itimes)
+            {
+                TimeSpan diff = t - previous;
+                differences.Add(diff);
+                previous = t;
+            }
+
+            bool swapper = false;
+            foreach (TimeSpan t in differences)
+            {
+                double chunkWidth = chartWidth * t.Ticks / most.Ticks;
+                InfoPanel.Children.Add(new Rectangle() { Width = chunkWidth, Height = 6, Fill = swapper ? Brushes.Peru : Brushes.LightYellow });
+                swapper = !swapper;
+            }
+
+            List<TimeSpan> btimes = Kernel.Instance.JobProfiles.JbhBusinessAgos();
+            most = btimes.Max();
+
+            differences = new List<TimeSpan>();
+            previous = TimeSpan.Zero;
+            foreach (TimeSpan t in btimes)
+            {
+                TimeSpan diff = t - previous;
+                differences.Add(diff);
+                previous = t;
+            }
+
+            swapper = false;
+            foreach (TimeSpan t in differences)
+            {
+                double chunkWidth = chartWidth * t.Ticks / most.Ticks;
+                BusinessPanel.Children.Add(new Rectangle() { Width = chunkWidth, Height = 6, Fill = swapper ? Brushes.Peru : Brushes.LightYellow });
+                swapper = !swapper;
+            }
+        }
+}
