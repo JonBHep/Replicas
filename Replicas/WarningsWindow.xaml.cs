598277362c7a3c3656c21baaae80e642ddc69aca
Jonathan Hepworth <jonathanhepworth@hotmail.co.uk>
2022-01-13 15:21:30 +0000

    Added warnings window

---
+++ Replicas/WarningsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Replicas;
+
+internal partial class WarningsWindow : Window
+{
+    internal WarningsWindow()
+    {
+        InitializeComponent();
+    }
+    public void SetCaption(string txt)
+    {
+        this.Title = txt;
+    }
+
+    public void SetComment(string txt)
+    {
+        this.lblMessage.Text = txt;
+    }
+
+    public void AddToList(string txt)
+    {
+        this.lstExamples.Items.Add(txt);
+    }
+}
